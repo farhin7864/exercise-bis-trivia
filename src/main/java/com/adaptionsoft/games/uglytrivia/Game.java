e929a9573012a208928d4a83e74d3e2f259ce06e
farhin7864 <farhin.nadaf786@gmail.com>
2023-08-15 16:05:45 +0530

    Challenge completed.!

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -34,23 +34,110 @@ public class Game {
 
 	public Game() {
 
-		// Add questions
-		addQuestionAndAnswers();
-
-		
-		/*
-		 * for (int i = 0; i < 2; i++) { popQuestions.addLast("Pop Question " + i);
-		 * scienceQuestions.addLast(("Science Question " + i));
-		 * sportsQuestions.addLast(("Sports Question " + i));
-		 * rockQuestions.addLast(createRockQuestion(i)); }
-		 */
-		 
+		//Load Question and Answers
+		loadQuestionsAndAnswers();
 	}
 
-	private void addQuestionAndAnswers() {
+	private void loadQuestionsAndAnswers() {
+		getSportsQA();
+		getSceinceQA();
+		getRockQA();
+		getPopQA();
+					
 
-// Sports
+	}
+     
+
+	/**
+	 * Description : Gives pop question and answers
+	 */
+	private void getPopQA() {
+		popQuestions.addAll(Arrays.asList(
+			    "What is the smallest planet in our solar system?",
+			    "What sport did David Beckham play?",
+			    "What has Mohammed Ali’s original name?",
+			    "What does the Latin word 'tempus' mean in English?",
+			    "What is the smallest planet in our solar system?",
+			    "What sport did David Beckham play?",
+			    "What has Mohammed Ali’s original name?",
+			    "What does the Latin word 'tempus' mean in English?"
+			));
+
+			popAnswers.addAll(Arrays.asList(
+			    "Mercury",
+			    "Football",
+			    "Boxing",
+			    "Time",
+			    "Mercury",
+			    "Football",
+			    "Boxing",
+			    "Time"
+			));
 		
+	}
+
+	/**
+	 * Description : Gives rock question and answers
+	 */
+	private void getRockQA() {
+		rockQuestions.addAll(Arrays.asList(
+			    "Which is the largest country in the world?",
+			    "Which country has the largest population in the world?",
+			    "In which country would you find the Leaning Tower of Pisa?",
+			    "Which planet is nearest to the Earth??",
+			    "Which is the largest country in the world?",
+			    "Which country has the largest population in the world?",
+			    "In which country would you find the Leaning Tower of Pisa?",
+			    "Which planet is nearest to the Earth??"
+			));
+
+			rockAnswers.addAll(Arrays.asList(
+			    "Russia",
+			    "China",
+			    "Italy",
+			    "Venus",
+			    "Russia",
+			    "China",
+			    "Italy",
+			    "Venus"
+			));
+
+		
+	}
+
+	/**
+	 * Description : Gives science question and answers
+	 */
+	private void getSceinceQA() {
+		scienceQuestions.addAll(Arrays.asList(
+			    "Which animal lays eggs?",
+			    "A male cow is called?",
+			    "All animals need food, air, and ____ to survive.",
+			    "Which one is a fur-bearing animal?",
+			    "Which animal lays eggs?",
+			    "A male cow is called?",
+			    "All animals need food, air, and ____ to survive.",
+			    "Which one is a fur-bearing animal?"
+			));
+
+			scienceAnswers.addAll(Arrays.asList(
+			    "Duck",
+			    "Ox",
+			    "Water",
+			    "Cat",
+			    "Duck",
+			    "Ox",
+			    "Water",
+			    "Cat"
+			));
+
+		
+	}
+
+	/**
+	 * Description : Gives sports question and answers
+	 */
+	private void getSportsQA() {
 		sportsQuestions.addAll(Arrays.asList(
 			    "Which sport has 11 playes on the field?",
 			    "Which sport uses a net, a racket, and a shuttlecock?",
@@ -73,81 +160,7 @@ public class Game {
 			    String.valueOf(5)
 			));
 
-//Science
-		scienceQuestions.add("Which animal lays eggs?");
-		scienceAnswers.add("Duck");
-
-		scienceQuestions.add("A male cow is called?");
-		scienceAnswers.add(1, "Ox");
-
-		scienceQuestions.add("All animals need food, air, and ____ to survive.");
-		scienceAnswers.add("Water");
-
-		scienceQuestions.add("Which one is a fur-bearing animal?");
-		scienceAnswers.add("Cat");
-
-		scienceQuestions.add("Which animal lays eggs?");
-		scienceAnswers.add("Duck");
-
-		scienceQuestions.add("A male cow is called?");
-		scienceAnswers.add("Ox");
-
-		scienceQuestions.add("All animals need food, air, and ____ to survive.");
-		scienceAnswers.add("Water");
-
-		scienceQuestions.add("Which one is a fur-bearing animal?");
-		scienceAnswers.add("Cat");
-
-// Rock
-		rockQuestions.add("Which is the largest country in the world?");
-		rockAnswers.add("Russia");
-
-		rockQuestions.add("Which country has the largest population in the world?");
-		rockAnswers.add("China");
-
-		rockQuestions.add("In which country would you find the Leaning Tower of Pisa?");
-		rockAnswers.add("Italy");
-
-		rockQuestions.add("Which planet is nearest to the Earth??");
-		rockAnswers.add("Venus");
-
-		rockQuestions.add("Which is the largest country in the world?");
-		rockAnswers.add("Russia");
-
-		rockQuestions.add("Which country has the largest population in the world?");
-		rockAnswers.add("China");
-
-		rockQuestions.add("In which country would you find the Leaning Tower of Pisa?");
-		rockAnswers.add("Italy");
-
-		rockQuestions.add("Which planet is nearest to the Earth??");
-		rockAnswers.add("Venus");
-
-//Pop
-		popQuestions.add("What is the smallest planet in our solar system?");
-		popAnswers.add("Mercury");
-
-		popQuestions.add("What sport did David Beckham play?");
-		popAnswers.add("Football");
-
-		popQuestions.add("What has Mohammed Ali’s original name?");
-		popAnswers.add("Boxing");
-
-		popQuestions.add("What does the Latin word 'tempus' mean in English?");
-		popAnswers.add("Time");
 		
-		popQuestions.add("What is the smallest planet in our solar system?");
-		popAnswers.add("Mercury");
-
-		popQuestions.add("What sport did David Beckham play?");
-		popAnswers.add("Football");
-
-		popQuestions.add("What has Mohammed Ali’s original name?");
-		popAnswers.add("Boxing");
-
-		popQuestions.add("What does the Latin word 'tempus' mean in English?");
-		popAnswers.add("Time");
-
 	}
 
 	public String createRockQuestion(int index) {
@@ -158,6 +171,11 @@ public class Game {
 		return (howManyPlayers() >= 2);
 	}
 
+	/**
+	 * Description: Adds the player in the Game.
+	 * @param playerName
+	 * @return returns true
+	 */
 	public boolean add(String playerName) {
 
 		players.add(playerName);
@@ -170,10 +188,20 @@ public class Game {
 		return true;
 	}
 
+	/**
+	 * @return returns the total number of player
+	 */
 	public int howManyPlayers() {
 		return players.size();
 	}
 
+	/**
+	 * Description: This method roll(int roll) gets the number generated by the Random.rand.nextInt() and 
+	 *              throws the question based on place and category.
+	 *              It internally checks the penalty of the user also if dice is even player will not come
+	 *              out of Penalty box. Only if in penalty as well dice is odd player will be moved out.
+	 * @param roll - 
+	 */
 	public void roll(int roll) {
 		System.out.println(players.get(currentPlayer) + " is the current player");
 		System.out.println("They have rolled a " + roll);
@@ -208,43 +236,6 @@ public class Game {
 
 	}
 
-	/*
-	 * private void askQuestion() { if (currentCategory() == "Pop") { index =
-	 * popQuestions.indexOf(popQuestions.size()) + 1; strQuestion =
-	 * popQuestions.removeFirst(); System.out.println(strQuestion); inputFromUser =
-	 * sc.next(); //Store answer
-	 * 
-	 * validateAnswer(currentCategory(), index, inputFromUser);
-	 * 
-	 * } if (currentCategory() == "Science") { index =
-	 * scienceAnswers.indexOf(scienceQuestions.size()) + 1; String strQuestion =
-	 * scienceQuestions.removeFirst(); System.out.println(strQuestion);
-	 * inputFromUser = sc.next(); validateAnswer(currentCategory(), index,
-	 * inputFromUser); } if (currentCategory() == "Sports") { index =
-	 * sportsQuestions.indexOf(sportsQuestions.size()) + 1; String strQuestion =
-	 * sportsQuestions.removeFirst(); System.out.println(strQuestion); inputFromUser
-	 * = sc.next(); validateAnswer(currentCategory(), index, inputFromUser); } if
-	 * (currentCategory() == "Rock") { index =
-	 * rockAnswers.indexOf(rockQuestions.size()) + 1; String strQuestion =
-	 * rockQuestions.removeFirst(); System.out.println(strQuestion); inputFromUser =
-	 * sc.next(); validateAnswer(currentCategory(), index, inputFromUser); } }
-	 * 
-	 * // Validate the input answer and call for existing methods. private boolean
-	 * validateAnswer(String strCategory, int index, String inputFromUser) {
-	 * 
-	 * if (strCategory.equalsIgnoreCase("Pop")) if
-	 * (popAnswers.contains(inputFromUser)) { return true; }
-	 * 
-	 * if (strCategory.equalsIgnoreCase("Rock")) if
-	 * (rockAnswers.contains(inputFromUser)) { return true; }
-	 * 
-	 * if (strCategory.equalsIgnoreCase("Science")) if
-	 * (scienceAnswers.contains(inputFromUser))//.get(index).equals(inputFromUser))
-	 * { { return true; }
-	 * 
-	 * if (strCategory.equalsIgnoreCase("Sports")) if
-	 * (sportsAnswers.contains(inputFromUser)){ return true; } return false; }
-	 */
 	public String currentCategory() {
 		if (places[currentPlayer] == 0)
 			return "Pop";
@@ -267,6 +258,13 @@ public class Game {
 		return "Rock";
 	}
 
+	//
+	/**
+	 * Description: It prints the correct answer message and checks if player is eligible for winning if yes 
+	 *              then declares winner and exit.
+	 *              If not winner yet, move to next player.
+	 * @return true
+	 */
 	public boolean wasCorrectlyAnswered() {
 		if (inPenaltyBox[currentPlayer]) {
 			if (isGettingOutOfPenaltyBox) {
@@ -303,6 +301,12 @@ public class Game {
 		}
 	}
 
+	
+	/**
+	 * Description: It prints incorrect message and put player in penalty box.
+	 *              and moves to next player.
+	 * @return
+	 */
 	public boolean wrongAnswer() {
 		System.out.println("Question was incorrectly answered");
 		System.out.println(players.get(currentPlayer) + " was sent to the penalty box");
@@ -314,11 +318,15 @@ public class Game {
 		return true;
 	}
 
+	
 	private boolean didPlayerWin() {
 		return !(purses[currentPlayer] == 6);
 	}
 	
 	
+	/**
+	 * Description: Post the question to player. 
+	 */
 	private void askQuestion() {
 	    String currentCategory = currentCategory();
 	    String currentQuestion = "";
@@ -345,6 +353,13 @@ public class Game {
 	    }
 	}
 
+	
+	/**
+	 * @param strCategory - takes the category
+	 * @param question - takes the question
+	 * @param inputFromUser - takes the user input
+	 * @return true if answer is correct else false.
+	 */
 	public boolean validateAnswer(String strCategory, String question, String inputFromUser) {
 	  
 	    if (strCategory.equalsIgnoreCase("Pop")) {
